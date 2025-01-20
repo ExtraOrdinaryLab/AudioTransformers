ce73fba4e6fbd2fb620e2feb3eb5e97260321bae
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2025-01-20 19:24:34 +0000

    [feat] add AudioTransformerDataCollator

--- src/audio_transformers/trainer.py
+++ src/audio_transformers/trainer.py
@@ -1,17 +1,24 @@
 import os
+import math
+import random
 import warnings
+from contextlib import nullcontext
+from dataclasses import dataclass, field
 from typing import List, Union, Dict, Any, Callable, Tuple, Optional, TYPE_CHECKING
 
 import torch
 import torch.nn as nn
 import huggingface_hub.utils as hf_hub_utils
+from sklearn.linear_model import LogisticRegression
 from packaging.version import parse as parse_version
 from transformers import __version__ as transformers_version
 from transformers import Trainer, TrainerCallback
 from transformers.data.data_collator import DataCollator
 from transformers.integrations import WandbCallback
 from transformers.trainer_callback import TrainerState
+from transformers.trainer_utils import EvalLoopOutput
 from transformers.utils import is_sagemaker_mp_enabled
+from transformers.integrations.tpu import tpu_spmd_dataloader
 from transformers.trainer_utils import (
     EvalPrediction,
     enable_full_determinism,
@@ -24,6 +31,9 @@ from torch.utils.data import BatchSampler, ConcatDataset, DataLoader, SubsetRand
 
 from . import logging, losses
 from . import AudioTransformer
+from .util import disable_logging, batch_to_device
+from .evaluation import AudioEvaluator
+from .sampler import NoDuplicatesBatchSampler
 from .training_args import AudioTransformerTrainingArguments, BatchSamplers
 
 logging.set_verbosity_info()
@@ -42,22 +52,49 @@ if TYPE_CHECKING:
     import optuna
 
 
+@dataclass
 class AudioTransformerDataCollator(object):
 
-    def __init__(self, audio_transformer: AudioTransformer):
-        self.audio_transformer = audio_transformer
+    featurizer: Callable
+    valid_label_columns: list[str] = field(default_factory=lambda: ["label", "score"])
 
-    def __call__(self, batch: List[Dict[str, str]]):
-        features = {}
-        columns = list(batch[0].keys())
-        for column in columns:
-            if column in ['label', 'labels']:
-                features['label'] = [example[column] for example in batch]
-            else:
-                audios_batch = [example[column] for example in batch]
-                features[column] = self.audio_transformer.transformer_model.featurize(audios_batch)
-        return features
-    
+    # def __init__(self, audio_transformer: AudioTransformer):
+    #     self.audio_transformer = audio_transformer
+    #     self.valid_label_columns = ['label', 'labels', 'audio_1', 'audio_2']
+
+    # def __call__(self, batch: List[Dict[str, Any]]):
+    #     features = {}
+    #     columns = list(batch[0].keys())
+    #     for column in columns:
+    #         if column in ['label', 'labels']:
+    #             features['label'] = [example[column] for example in batch]
+    #         else:
+    #             audios_batch = [example[column] for example in batch]
+    #             features[column] = self.audio_transformer.transformer_model.featurize(audios_batch)
+    #     return features
+
+    def __call__(self, features: List[Dict[str, Any]]):
+        column_names = list(features[0].keys())
+
+        batch = {}
+
+        if "dataset_name" in column_names:
+            column_names.remove("dataset_name")
+            batch["dataset_name"] = features[0]["dataset_name"]
+
+        # Extract the label column if it exists
+        for label_column in self.valid_label_columns:
+            if label_column in column_names:
+                batch["label"] = torch.tensor([row[label_column] for row in features])
+                column_names.remove(label_column)
+                break
+
+        for column_name in column_names:
+            audios_batch = [row[column_name] for row in features]
+            batch[column_name] = self.featurizer(audios_batch)
+        
+        return batch
+
 
 class AudioTransformerTrainer(Trainer):
 
@@ -70,6 +107,7 @@ class AudioTransformerTrainer(Trainer):
         compute_metrics: Optional[Callable[[EvalPrediction, Dict[str, Any]], Dict[str, float]]] = None,
         data_collator: Callable = None, 
         loss_fn: Union[nn.Module, Callable[[AudioTransformer], nn.Module], Dict[str, Callable[[AudioTransformer], nn.Module]]] = None, 
+        evaluator = None,
         callbacks: List[TrainerCallback] = None,
         optimizers: Tuple[torch.optim.Optimizer, torch.optim.lr_scheduler.LambdaLR] = (None, None),
         preprocess_logits_for_metrics: Callable[[torch.Tensor, torch.Tensor], torch.Tensor] = None,
@@ -93,7 +131,9 @@ class AudioTransformerTrainer(Trainer):
         default_args_dict = AudioTransformerTrainingArguments(output_dir="unused").to_dict()
 
         if data_collator is None:
-            data_collator = AudioTransformerDataCollator(model)
+            data_collator = AudioTransformerDataCollator(
+                featurizer=model.transformer_model.featurize
+            )
 
         for dataset_name, dataset in zip(["train", "eval"], [train_dataset, eval_dataset]):
             if isinstance(dataset, IterableDataset) and dataset.column_names is None:
@@ -153,6 +193,8 @@ class AudioTransformerTrainer(Trainer):
             loss_fn = losses.MultipleNegativesRankingLoss(self.model)
         self.loss_fn = self.prepare_loss(loss_fn, model)
 
+        self.evaluator = evaluator
+
     def prepare_loss(
         self,
         loss: Union[Callable[[AudioTransformer], torch.nn.Module], torch.nn.Module],
@@ -200,9 +242,18 @@ class AudioTransformerTrainer(Trainer):
             "pin_memory": self.args.dataloader_pin_memory,
             "persistent_workers": self.args.dataloader_persistent_workers,
             "prefetch_factor": self.args.dataloader_prefetch_factor,
-            "batch_size": self.args.train_batch_size,
-            "drop_last": self.args.dataloader_drop_last,
+            # "batch_size": self.args.train_batch_size,
+            # "drop_last": self.args.dataloader_drop_last,
         }
+
+        batch_sampler = NoDuplicatesBatchSampler(
+            dataset=train_dataset,
+            batch_size=self.args.train_batch_size,
+            drop_last=self.args.dataloader_drop_last,
+            valid_label_columns=self.data_collator.valid_label_columns,
+            generator=generator,
+        )
+        dataloader_params["batch_sampler"] = batch_sampler
 
         # If 'even_batches' is True, it will use the initial few samples to pad out the last sample. This can
         # cause issues with multi-dataset training, so we want to set this to False.
@@ -307,6 +358,38 @@ class AudioTransformerTrainer(Trainer):
                 self._move_model_to_device(self.model, args.device)
             self.model_wrapped = self.model
 
+        train_dataloader = self.get_train_dataloader()
+        if self.is_fsdp_xla_v2_enabled:
+            train_dataloader = tpu_spmd_dataloader(train_dataloader)
+        len_dataloader = len(train_dataloader)
+        num_update_steps_per_epoch = len_dataloader // args.gradient_accumulation_steps
+        num_update_steps_per_epoch = max(num_update_steps_per_epoch, 1)
+        num_examples = self.num_examples(train_dataloader)
+        total_train_batch_size = self._train_batch_size * args.gradient_accumulation_steps * args.world_size
+
+        if args.max_steps > 0:
+            max_steps = args.max_steps
+            num_train_epochs = args.max_steps // num_update_steps_per_epoch + int(
+                args.max_steps % num_update_steps_per_epoch > 0
+            )
+            # May be slightly incorrect if the last batch in the training dataloader has a smaller size but it's
+            # the best we can do.
+            num_train_samples = args.max_steps * total_train_batch_size
+        else:
+            max_steps = math.ceil(args.num_train_epochs * num_update_steps_per_epoch)
+            num_train_epochs = math.ceil(args.num_train_epochs)
+            num_train_samples = self.num_examples(train_dataloader) * args.num_train_epochs
+
+        logger.info("***** Running training *****")
+        logger.info(f"  Num unique pairs = {len(self.train_dataset)}")
+        logger.info(f"  Num Epochs = {num_train_epochs}")
+        logger.info(f"  Num GPUs = {args.n_gpu}")
+        logger.info(f"  World size = {args.world_size}")
+        logger.info(f"  Instantaneous batch size per device = {args.per_device_train_batch_size}")
+        logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {self._train_batch_size}")
+        logger.info(f"  Gradient Accumulation steps = {args.gradient_accumulation_steps}")
+        logger.info(f"  Total optimization steps = {max_steps}")
+
         inner_training_loop = find_executable_batch_size(
             self._inner_training_loop, self._train_batch_size, args.auto_find_batch_size
         )
@@ -330,9 +413,60 @@ class AudioTransformerTrainer(Trainer):
                 ignore_keys_for_eval=ignore_keys_for_eval,
             )
 
+    def evaluate(
+        self,
+        eval_dataset: Union[Dataset, Dict[str, Dataset]] = None,
+        ignore_keys: List[str] = None,
+        metric_key_prefix: str = "eval",
+    ) -> Dict[str, float]:
+        eval_dataset = self.eval_dataset
+        return super().evaluate(eval_dataset, ignore_keys, metric_key_prefix)
 
-def batch_to_device(batch: Dict[str, Any], target_device: str) -> dict[str, Any]:
-    for key in batch:
-        if isinstance(batch[key], torch.Tensor):
-            batch[key] = batch[key].to(target_device)
-    return batch
+    def evaluation_loop(
+        self,
+        dataloader: DataLoader,
+        description: str,
+        prediction_loss_only: bool = None,
+        ignore_keys: list[str] = None,
+        metric_key_prefix: str = "eval",
+    ) -> EvalLoopOutput:
+        output = super().evaluation_loop(
+            dataloader=dataloader,
+            description=description,
+            prediction_loss_only=prediction_loss_only,
+            ignore_keys=ignore_keys,
+            metric_key_prefix=metric_key_prefix,
+        )
+
+        # If the evaluator is not defined, we can just return the output
+        if self.evaluator is None:
+            return output
+
+        # If we are training and eval_dataset is a DatasetDict, then we should
+        # 1) only run the evaluator for the first dataset
+        # 2) prefix that only run as "eval", rather than e.g. "eval_multi_nli"
+        if self.is_in_train and isinstance(self.eval_dataset, dict) and metric_key_prefix.startswith("eval_"):
+            if metric_key_prefix[5:] == list(self.eval_dataset.keys())[0]:
+                metric_key_prefix = "eval"
+            else:
+                return output
+
+        with nullcontext() if self.is_local_process_zero() else disable_logging(logging.INFO):
+            output_path = self.args.output_dir
+            if output_path is not None:
+                output_path = os.path.join(output_path, "eval")
+                os.makedirs(output_path, exist_ok=True)
+            evaluator_metrics = self.evaluator(
+                self.model, output_path=output_path, epoch=self.state.epoch, steps=self.state.global_step
+            )
+        if not isinstance(evaluator_metrics, dict):
+            evaluator_metrics = {"evaluator": evaluator_metrics}
+
+        # Prefix all keys with metric_key_prefix + '_'
+        for key in list(evaluator_metrics.keys()):
+            if not key.startswith(f"{metric_key_prefix}_"):
+                evaluator_metrics[f"{metric_key_prefix}_{key}"] = evaluator_metrics.pop(key)
+
+        output.metrics.update(evaluator_metrics)
+
+        return output
